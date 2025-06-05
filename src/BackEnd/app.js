dffd38d37149b6ae3f28d66ea34c20c709cd858e
Antoine Gadrat <antoinegadrat@hotmail.fr>
2025-06-05 13:35:47 +0200

    feat: prepared mongoDb plus backend files

--- src/BackEnd/app.js
+++ src/BackEnd/app.js
@@ -2,6 +2,23 @@ const express = require('express');
 
 const app = express();
 
+const mongoose = require('mongoose');
+
+app.use(express.json());
+mongoose.connect('mongodb+srv://AntoineTCG29:Vu0jF8myADaZn8By@cluster0-pme76.mongodb.net/test?retryWrites=true&w=majority',
+    { useNewUrlParser: true,
+      useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+  
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
 app.use((req, res) => {
    res.json({ message: 'Votre requête a bien été reçue !' }); 
 });
